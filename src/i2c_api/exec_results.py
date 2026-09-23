55656dee07f2423f04f8f7eab2af65c45304fffb
Dmitri Priimak <priimak@gmail.com>
2026-09-23 16:17:28 -0500

    Added ExecResults::values() and pp() pretty print function.

--- src/i2c_api/exec_results.py
+++ src/i2c_api/exec_results.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from typing import NamedTuple
 
 from bitstring import BitArray
@@ -6,3 +7,12 @@ from bitstring import BitArray
 class ExecResults(NamedTuple):
     data: list[list[BitArray]]
     is_success: bool
+
+    def values(self) -> list[BitArray]:
+        """Returns contained response data as a list of BitArray with consecutive bytes combined"""
+        return [reduce(lambda v, acc: v + acc, [BitArray(f"uint:8={x}") for x in rsp], BitArray()) for rsp in self.data]
+
+    def pp(self) -> str:
+        """Pretty print"""
+        values_ = ["0b" + v.bin for v in self.values()]
+        return f"ExecResults(values={values_}, is_success={self.is_success})"
